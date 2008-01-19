c2e1480323223081f39171342b48f36767c70b56
rsaccon <rsaccon@21f08b38-4d3e-0410-bd9f-0fb83bf2c4e3>
2008-01-19 03:38:16 +0000

    housekeeping
    git-svn-id: http://erlyjs.googlecode.com/svn/trunk@14 21f08b38-4d3e-0410-bd9f-0fb83bf2c4e3

---
+++ src/demo/erlyjs_demo.erl
@@ -0,0 +1,68 @@
+%%%-------------------------------------------------------------------
+%%% File:      erlyjs_demo.erl
+%%% @author    Roberto Saccon <rsaccon@gmail.com> [http://rsaccon.com]
+%%% @copyright 2007 Roberto Saccon
+%%% @doc       ErlyJS demo application
+%%% @end
+%%%
+%%% The MIT License
+%%%
+%%% Copyright (c) 2007 Roberto Saccon
+%%%
+%%% Permission is hereby granted, free of charge, to any person obtaining a copy
+%%% of this software and associated documentation files (the "Software"), to deal
+%%% in the Software without restriction, including without limitation the rights
+%%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%%% copies of the Software, and to permit persons to whom the Software is
+%%% furnished to do so, subject to the following conditions:
+%%%
+%%% The above copyright notice and this permission notice shall be included in
+%%% all copies or substantial portions of the Software.
+%%%
+%%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+%%% THE SOFTWARE.
+%%%
+%%% @since 2007-12-14 by Roberto Saccon
+%%%-------------------------------------------------------------------
+-module(erlyjs_demo).
+-author('rsaccon@gmail.com').
+
+%% API
+-export([test/0, test/1]).
+
+%%====================================================================
+%% API
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% @spec () -> Ast::tuple()
+%% @doc
+%% @end 
+%%--------------------------------------------------------------------
+test() ->        
+    Dir = filename:join([filename:dirname(code:which(?MODULE)),"..", "demo"]),
+    test(filename:join([Dir, "test.js"])).
+
+ 
+%%--------------------------------------------------------------------
+%% @spec (string()) -> Ast::tuple()
+%% @doc
+%% @end 
+%%--------------------------------------------------------------------
+test(File) ->   
+	case file:read_file(File) of
+        {ok, Bin} ->
+            ErlAst = erlyjs_server:compile(Bin),
+            io:format("TRACE ~p:~p Erlang AST:~n ~p~n",[?MODULE, ?LINE, ErlAst]);
+		_ ->
+		    io:format("TRACE ~p:~p reading js sourcefile failed~n",[?MODULE, ?LINE])
+	end.
+	
+%%====================================================================
+%% Internal functions
+%%====================================================================
