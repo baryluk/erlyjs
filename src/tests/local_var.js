a6ea3a88da9449f0e82d045a2da01b0417f2503e
rsaccon <rsaccon@21f08b38-4d3e-0410-bd9f-0fb83bf2c4e3>
2008-02-06 05:05:58 +0000

    added some tests and improved testsuite
    git-svn-id: http://erlyjs.googlecode.com/svn/trunk@42 21f08b38-4d3e-0410-bd9f-0fb83bf2c4e3

---
+++ src/tests/local_var.js
@@ -0,0 +1,16 @@
+function test_description() {
+    return "Simple local variable test";
+}
+
+function test_args() {
+    return "";
+}
+
+function test_result() {
+    return 5;
+}
+
+function test() {
+    var a = 5;
+    return a;
+}
