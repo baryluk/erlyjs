decb48bc33d648921f8bbf994dcc3da276fe4f15
rsaccon <rsaccon@21f08b38-4d3e-0410-bd9f-0fb83bf2c4e3>
2008-02-09 06:12:53 +0000

    added comments test
    git-svn-id: http://erlyjs.googlecode.com/svn/trunk@64 21f08b38-4d3e-0410-bd9f-0fb83bf2c4e3

---
+++ src/tests/comments.js
@@ -0,0 +1,38 @@
+// Mandatory. Provide here a description of the test case.
+function test_description() {
+    return "";
+}
+
+// Mandatory. Provide here the arguments the testsuite will use 
+// to invoke the test() function.
+function test_args() {
+    return [];
+}
+
+// Mandatory. Provide here the expected test result. 
+function test_assert() {
+    return 6;
+}
+
+// Optional. Provide here any global code.
+
+
+// Mandatory. The actual test. 
+// Testsuite invokes this function with the arguments from test_args()
+// and compares the return value with the expected result from test_result().
+function test() {
+    var a=5;
+    
+    // a = a + 1;
+    
+    a = a + 1;  // a = a + 1;
+    
+/*===============  
+    a = a + 1;
+    a = a + 1;  
+=================*/
+    
+    /* a = a + 1; */
+    
+    return a;
+}
