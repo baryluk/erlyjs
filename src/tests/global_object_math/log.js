48b6bcc7dc150b15c5156d007b3c0fc0a8da0513
rsaccon <rsaccon@21f08b38-4d3e-0410-bd9f-0fb83bf2c4e3>
2008-02-29 16:32:23 +0000

    more Math tests
    git-svn-id: http://erlyjs.googlecode.com/svn/trunk@114 21f08b38-4d3e-0410-bd9f-0fb83bf2c4e3

---
+++ src/tests/global_object_math/log.js
@@ -0,0 +1,26 @@
+// Mandatory. Provide here a description of the test case.
+function test_description() {
+    return "Prediefined core object 'Math', method: 'log'";
+}
+
+// Mandatory. Provide here the arguments the testsuite will use 
+// to invoke the test() function.
+function test_args() {
+    return [];
+}
+
+// Mandatory. Provide here the expected test result. 
+function test_ok() {
+    return 0.6931471805599453;
+}
+
+// Optional. Provide here any global code.
+
+
+// Mandatory. The actual test. 
+// Testsuite invokes this function with the arguments from test_args()
+// and compares the return value with the expected result from test_result().
+function test() {
+    return Math.log(2);
+}
+
---
+++ src/tests/global_object_math/pow.js
@@ -0,0 +1,25 @@
+// Mandatory. Provide here a description of the test case.
+function test_description() {
+    return "Prediefined core object 'Math', method: 'pow'";
+}
+
+// Mandatory. Provide here the arguments the testsuite will use 
+// to invoke the test() function.
+function test_args() {
+    return [];
+}
+
+// Mandatory. Provide here the expected test result. 
+function test_ok() {
+    return 8;
+}
+
+// Optional. Provide here any global code.
+
+
+// Mandatory. The actual test. 
+// Testsuite invokes this function with the arguments from test_args()
+// and compares the return value with the expected result from test_result().
+function test() {
+    return Math.pow(2, 3);
+}
